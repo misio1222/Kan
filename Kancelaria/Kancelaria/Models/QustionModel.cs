bf8aa016b9b92d231e4f90f16b4d854ff2c263fd
misio1222 <misio1222@o2.pl>
2017-01-12 20:24:40 +0100

    red

---
+++ Kancelaria/Kancelaria/Models/QustionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kancelaria.Models
+{
+    public class QustionModel
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public string emailAdress { get; set; }
+        public string question { get; set; }
+    }
+}
